0e26fc730d99239f348004fe36de013b1f4ccf59
l930617673 <56494145+l930617673@users.noreply.github.com>
2020-07-02 09:39:19 +0800

    优化下载体验，优化加载速度

---
+++ app/src/main/java/top/lhjjjlxays/appstore/bean/ApkDetail.java
@@ -0,0 +1,92 @@
+package top.lhjjjlxays.appstore.bean;
+
+import java.util.List;
+
+public class ApkDetail {
+    private String apk_developer;       //开发商
+    private String evaluate_number;     //评论次数
+    private String update_date;         //更新时间
+    private String package_name;        //包名must
+    private String apk_permission;      //权限
+    private List<String> apk_screenshots; //截图
+    private String apk_introduce;       //简介
+    private String version_feature;     //版本特性
+
+    public ApkDetail() {
+    }
+
+    public ApkDetail(String apk_developer, String evaluate_number, String update_date, String package_name, String apk_permission, List<String> apk_screenshots, String apk_introduce, String version_feature) {
+        this.apk_developer = apk_developer;
+        this.evaluate_number = evaluate_number;
+        this.update_date = update_date;
+        this.package_name = package_name;
+        this.apk_permission = apk_permission;
+        this.apk_screenshots = apk_screenshots;
+        this.apk_introduce = apk_introduce;
+        this.version_feature = version_feature;
+    }
+
+    public String getApk_developer() {
+        return apk_developer;
+    }
+
+    public void setApk_developer(String apk_developer) {
+        this.apk_developer = apk_developer;
+    }
+
+    public String getEvaluate_number() {
+        return evaluate_number;
+    }
+
+    public void setEvaluate_number(String evaluate_number) {
+        this.evaluate_number = evaluate_number;
+    }
+
+    public String getUpdate_date() {
+        return update_date;
+    }
+
+    public void setUpdate_date(String update_date) {
+        this.update_date = update_date;
+    }
+
+    public String getPackage_name() {
+        return package_name;
+    }
+
+    public void setPackage_name(String package_name) {
+        this.package_name = package_name;
+    }
+
+    public String getApk_permission() {
+        return apk_permission;
+    }
+
+    public void setApk_permission(String apk_permission) {
+        this.apk_permission = apk_permission;
+    }
+
+    public List<String> getApk_screenshots() {
+        return apk_screenshots;
+    }
+
+    public void setApk_screenshots(List<String> apk_screenshots) {
+        this.apk_screenshots = apk_screenshots;
+    }
+
+    public String getApk_introduce() {
+        return apk_introduce;
+    }
+
+    public void setApk_introduce(String apk_introduce) {
+        this.apk_introduce = apk_introduce;
+    }
+
+    public String getVersion_feature() {
+        return version_feature;
+    }
+
+    public void setVersion_feature(String version_feature) {
+        this.version_feature = version_feature;
+    }
+}
---
+++ app/src/main/java/top/lhjjjlxays/appstore/bean/ApkGeneral.java
@@ -0,0 +1,98 @@
+package top.lhjjjlxays.appstore.bean;
+
+public class ApkGeneral {
+    private String apk_name;        //名称
+    private String apk_icon;        //icon
+    private String apk_grade;       //评分
+    private String apk_size;        //大小
+    private String apk_version;     //版本
+    private String download_url;    //下载地址
+    private String package_name;    //包名must
+    private String old_version;     //旧版本
+
+    public ApkGeneral() {
+    }
+
+    public ApkGeneral(String apk_name, String apk_icon, String apk_grade, String apk_size, String apk_version, String download_url, String package_name, String old_version) {
+        this.apk_name = apk_name;
+        this.apk_icon = apk_icon;
+        this.apk_grade = apk_grade;
+        this.apk_size = apk_size;
+        this.apk_version = apk_version;
+        this.download_url = download_url;
+        this.package_name = package_name;
+        this.old_version = old_version;
+    }
+
+    public String getOld_version() {
+        return old_version;
+    }
+
+    public void setOld_version(String old_version) {
+        this.old_version = old_version;
+    }
+
+    public String getApk_name() {
+        return apk_name;
+    }
+
+    public void setApk_name(String apk_name) {
+        this.apk_name = apk_name;
+    }
+
+    public String getApk_icon() {
+        return apk_icon;
+    }
+
+    public void setApk_icon(String apk_icon) {
+        this.apk_icon = apk_icon;
+    }
+
+    public String getApk_grade() {
+        return apk_grade;
+    }
+
+    public void setApk_grade(String apk_grade) {
+        this.apk_grade = apk_grade;
+    }
+
+    public String getApk_size() {
+        return apk_size;
+    }
+
+    public void setApk_size(String apk_size) {
+        this.apk_size = apk_size;
+    }
+
+    public String getApk_version() {
+        return apk_version;
+    }
+
+    public void setApk_version(String apk_version) {
+        this.apk_version = apk_version;
+    }
+
+    public String getDownload_url() {
+        return download_url;
+    }
+
+    public void setDownload_url(String download_url) {
+        this.download_url = download_url;
+    }
+
+    public String getPackage_name() {
+        return package_name;
+    }
+
+    public void setPackage_name(String package_name) {
+        this.package_name = package_name;
+    }
+
+    public int versionController() {
+        if (old_version != null) {
+            return old_version.compareTo(apk_version);
+        }
+
+        return Integer.MIN_VALUE;
+    }
+}
